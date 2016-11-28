b1f944cb6fde3c06fd71c0bd99ae95d0943cd26e
Saad <Saad@SAADTHETRAVELER>
2016-11-28 14:18:53 +0800

    Add project files.

---
+++ PrincessCruise/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PrincessCruise.Startup))]
+namespace PrincessCruise
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
